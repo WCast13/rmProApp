d986fc4434fc47e68a82eb78fd63ead8fadda06f
Will Castellano <wcastellano13@gmail.com>
2024-08-07 12:03:32 -0400

    Initial Commit

---
+++ rmProApp/rmProAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rmProAppApp.swift
+//  rmProApp
+//
+//  Created by William Castellano on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct rmProAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
