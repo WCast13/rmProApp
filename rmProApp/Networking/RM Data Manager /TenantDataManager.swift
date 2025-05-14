e36ae2742e1a2c288bc504e6b9e715017301f2c9
Will Castellano <wcastellano13@gmail.com>
2025-05-14 12:38:59 -0400

    Updated loading resident data... Will begin updating design elements. Rebuild Resident Detail View.

--- rmProApp/Networking/RM Data Manager /TenantDataManager.swift
+++ rmProApp/Networking/RM Data Manager /TenantDataManager.swift
@@ -26,6 +26,19 @@ class TenantDataManager: ObservableObject {
     
     private init() {}
     
+    
+    // Enum representing different tenant data categories that may be updated
+    enum TenantDataSection {
+        case leases
+        case contacts
+        case addresses
+        case charges
+        case payments
+        case paymentReversals
+        case recurringCharges
+        case userDefinedValues
+    }
+    
     // MARK: Temporary Timing Function
     // Temporary Function- time api calls
     private func timeAPICall<T>(_ label: String, _ operation: () async -> T) async -> (result: T, duration: TimeInterval) {
@@ -36,88 +49,102 @@ class TenantDataManager: ObservableObject {
         return (result, duration)
     }
     
-    
-    // MARK: Tenant Embeds/Fields
-    private let simpleEmbeds: [TenantEmbedOption] = [
-        .addresses, .addresses_AddressType, .balance, .color, .contacts, .contacts_PhoneNumbers, .contacts_PhoneNumbers_PhoneNumberType, .evictions, .evictions_EvictionOutcome, .evictions_EvictionWorkflowStage, .leases, .leases_Unit, .leases_Unit_UnitType, .leases_Unit_Addresses, .loans, .openBalance, .openPrepays, .openReceivables, .openReceivables_ChargeType, .paymentReversals ,  .recurringChargeSummaries, .securityDepositHeld, .securityDepositSummaries, .userDefinedValues, .vehicles
-    ]
-    
-    private let simpleFields: [TenantFieldOption] = [
-        .addresses, .balance, .colorID, .comment, .contacts, .evictionID, .evictions, .firstName, .lastName, .leases, .loans, .name, .openBalance, .openReceivables, .paymentReversals, .propertyID,  .recurringChargeSummaries, .securityDepositHeld, .securityDepositSummaries, .status, .tenantDisplayID, .tenantID, .updateDate, .updateUserID, .userDefinedValues, .vehicles
-    ]
-    
-    private let fullEmbeds: [TenantEmbedOption] = [
-        .addresses, .addresses_AddressType, .balance, .charges, .color, .contacts, .contacts_Addresses, .contacts_ContactType, .contacts_PhoneNumbers, .contacts_PhoneNumbers_PhoneNumberType, .contacts_UserDefinedValues, .evictions, .evictions_EvictionOutcome, .evictions_EvictionWorkflowStage, .history, .leases, .leases_Property, .leases_Unit, .leases_Unit_Property, .leases_Unit_Addresses, .leases_Unit_UnitType, .loans, .openBalance, .openPrepays, .openReceivables, .openReceivables_ChargeType, .payments, .paymentReversals, .primaryContact, .primaryContact_PhoneNumbers, .primaryContact_PhoneNumbers_PhoneNumberType,  .recurringChargeSummaries, .recurringChargeSummaries_ChargeType, .securityDepositHeld, .securityDepositSummaries, .tenantBills, .tenantChecks, .userDefinedValues, .vehicles
-    ]
-    
-    private let fullFields: [TenantFieldOption] = [
-        .addresses, .balance, .charges, .colorID, .comment, .contacts, .evictionID, .evictions, .firstName, .history, .historyEviction, .historyEvictionNotes, .lastName, .leases, .loans, .name, .openBalance, .openReceivables, .payments, .paymentReversals, .primaryContact, .propertyID,  .recurringChargeSummaries, .securityDepositHeld, .securityDepositSummaries, .status, .tenantDisplayID, .tenantID, .updateDate, .updateUserID, .userDefinedValues, .vehicles
-    ]
-    
-    private let transactionsEmbeds: [TenantEmbedOption] = [.charges, .charges_ChargeType, .payments, .paymentReversals]
-    private let transactionsFields: [TenantFieldOption] = [.charges, .payments, .paymentReversals]
-    
-    private let addressEmbeds: [TenantEmbedOption] = [.addresses, .addresses_AddressType]
-    private let addressFields: [TenantFieldOption] = [.addresses]
-    
     // MARK: Fetch Tenants- Haven/Pembroke
     func fetchTenants() async {
-        
-        let simpleEmbedsString = simpleEmbeds.map { $0.rawValue }.joined(separator: ",")
-        let simpleFieldsString = simpleFields.map { $0.rawValue }.joined(separator: ",")
-        
-        let filtersHaven = [
-            RMFilter(key: "PropertyID", operation: "eq", value: "3"),
-            RMFilter(key: "Status", operation: "ne", value: "Past")
-        ]
-        let filtersPembroke = [
-            RMFilter(key: "PropertyID", operation: "eq", value: "12"),
-            RMFilter(key: "Status", operation: "ne", value: "Past")
-        ]
-        
-        let pageSize = 20000
-        
-        let havenUrl = URLBuilder.shared.buildURL(endpoint: .tenants, embeds: simpleEmbedsString, fields: simpleFieldsString, filters: filtersHaven, pageSize: pageSize)
-        let pembrokeUrl = URLBuilder.shared.buildURL(endpoint: .tenants, embeds: simpleEmbedsString, fields: simpleFieldsString, filters: filtersPembroke, pageSize: pageSize)
-        
-        if let havenUrl = havenUrl, let pembrokeUrl = pembrokeUrl {
-            let (havenResult, havenDuration) = await timeAPICall("Haven tenants fetch") {
-                await RentManagerAPIClient.shared.request(url: havenUrl, responseType: [RMTenant].self) ?? []
-            }
-            
-            let (pembrokeResult, pembrokeDuration) = await timeAPICall("Pembroke tenants fetch") {
-                await RentManagerAPIClient.shared.request(url: pembrokeUrl, responseType: [RMTenant].self) ?? []
-            }
-            
-            havenTenants = havenResult
-            pembrokeTenants = pembrokeResult
-            
-            print("Haven tenants count: \(havenTenants.count), took \(String(format: "%.3f", havenDuration)) seconds")
-            print("Pembroke tenants count: \(pembrokeTenants.count), took \(String(format: "%.3f", pembrokeDuration)) seconds")
-        }
+    
+        await allTenants = fetchTenantBase()
+        await fetchSection(for: allTenants, embeds: TenantEmbeds.leaseEmbeds, fields: TenantFields.leaseFields, section: .leases)
+        await fetchSection(for: allTenants, embeds: TenantEmbeds.contactsEmbeds, fields: TenantFields.contactFields, section: .contacts)
+        await fetchSection(for: allTenants, embeds: TenantEmbeds.addressEmbeds, fields: TenantFields.addressFields, section: .addresses)
+//        await fetchSection(for: allTenants, embeds: TenantEmbeds.loanEmbeds, fields: TenantFields.loanFields, section: .loans)
         
         await UnitDataManager.shared.loadUnitsWithBasicData()
-        buildFilteredResidents()
+        buildRentIncreaseTenants()
+    }
+    
+    private func fetchTenantBase() async -> [RMTenant] {
+        let filters = [
+            RMFilter(key: "Status", operation: "ne", value: "Past")
+        ]
+        
+        guard let url = URLBuilder.shared.buildURL(endpoint: .tenants, filters: filters) else {
+            return []
+        }
+        
+        let (result, _) = await timeAPICall("Base fetch for tenants: ") {
+            await RentManagerAPIClient.shared.request(url: url, responseType: [RMTenant].self) ?? []
+        }
+        
+        return result
+    }
+    
+    private func fetchSection(for tenants: [RMTenant], embeds: [TenantEmbeds], fields: [TenantFields], section: TenantDataSection) async {
+        
+        let embedsString = embeds.map(\.rawValue).joined(separator: ",")
+        let fieldsString = fields.map(\.rawValue).joined(separator: ",")
+        
+        let filters = [
+            RMFilter(key: "Status", operation: "ne", value: "Past")
+        ]
+        
+        guard let url = URLBuilder.shared.buildURL(endpoint: .tenants, embeds: embedsString, fields: fieldsString, filters: filters) else {
+            print("❌ Failed to build section URL")
+            return
+        }
+        
+        let tenantData: [RMTenant] = await RentManagerAPIClient.shared.request(url: url, responseType: [RMTenant].self) ?? []
+        
+        for newData in tenantData {
+            mergeTenant(newData: newData, section: section)
+        }
+    }
+    
+    private func mergeTenant(newData: RMTenant, section: TenantDataSection) {
+        
+        guard let newID = newData.tenantID, let index = allTenants.firstIndex(where: { $0.tenantID == newID }) else { return }
+        var existing = allTenants[index]
+        
+        switch section {
+        case .leases:
+            existing.leases = newData.leases
+        case .contacts:
+            existing.contacts = newData.contacts
+        case .charges:
+            existing.charges = newData.charges
+        case .payments:
+            existing.payments = newData.payments
+        case .paymentReversals:
+            existing.paymentReversals = newData.paymentReversals
+        case .addresses:
+            existing.addresses = newData.addresses
+        case .recurringCharges:
+            existing.recurringChargeSummaries = newData.recurringChargeSummaries
+        case .userDefinedValues:
+            existing.udfs = newData.udfs
+        }
+        
+        allTenants[index] = existing
+        print(allTenants.count)
     }
     
     // MARK: Get Single Tenant- Details
     func fetchSingleTenant(tenantID: String) async -> RMTenant! {
-        let fullEmbedsString = fullEmbeds.map { $0.rawValue }.joined(separator: ",")
-        let fullFieldsString = fullFields.map { $0.rawValue }.joined(separator: ",")
+        let fullEmbedsString = TenantEmbeds.fullEmbeds.map { $0.rawValue }.joined(separator: ",")
+        let fullFieldsString = TenantFields.fullFields.map { $0.rawValue }.joined(separator: ",")
         
         let singleTenantUrl = URLBuilder.shared.buildURL(endpoint: .tenants, embeds: fullEmbedsString, fields: fullFieldsString, id: tenantID)
         
         if let url = singleTenantUrl {
-           
-        singleTenant = await RentManagerAPIClient.shared.request(url: url, responseType: RMTenant.self)
+            
+            singleTenant = await RentManagerAPIClient.shared.request(url: url, responseType: RMTenant.self)
             
         }
         return singleTenant
     }
     
     func fetchSingleTenantTransactions(tenantID: String) async -> RMTenant? {
-        let transactionsEmbeds: [TenantEmbedOption] = [.charges, .charges_ChargeType, .payments, .paymentReversals]
-        let transactionsFields: [TenantFieldOption] = [.charges, .payments, .paymentReversals]
+        let transactionsEmbeds: [TenantEmbeds] = [.charges, .charges_ChargeType, .payments, .paymentReversals]
+        let transactionsFields: [TenantFields] = [.charges, .payments, .paymentReversals]
         
         let transactionEmbedsString = transactionsEmbeds.map { $0.rawValue }.joined(separator: ",")
         let transactionFieldsString = transactionsFields.map { $0.rawValue }.joined(separator: ",")
@@ -129,8 +156,8 @@ class TenantDataManager: ObservableObject {
     }
     
     func fetchAddresses(tenantID: String) async -> RMTenant? {
-        let addressEmbeds: [TenantEmbedOption] = [.addresses, .addresses_AddressType]
-        let addressFields: [TenantFieldOption] = [.addresses]
+        let addressEmbeds: [TenantEmbeds] = [.addresses, .addresses_AddressType]
+        let addressFields: [TenantFields] = [.addresses]
         
         let addressEmbedsString = addressEmbeds.map { $0.rawValue }.joined(separator: ",")
         let addressFieldsString = addressFields.map { $0.rawValue }.joined(separator: ",")
@@ -141,19 +168,10 @@ class TenantDataManager: ObservableObject {
         
         return tenantAddresses
     }
-
-    // MARK: Combines Haven and Pembroke Residents
-    func buildFilteredResidents() {
-        allTenants = havenTenants + pembrokeTenants
-        print(("All Tenants Count: \(allTenants.count)"))
-
-        rentIncreaseTenants = buildRentIncreaseTenants()
-        
-    }
     
     // MARK: Generate Rent Increase Tenants for Mailing Labels
     // TODO: Need to Add Vacant Units to List
-    func buildRentIncreaseTenants() -> [WCRentIncreaseTenant] {
+    func buildRentIncreaseTenants() {
         var rentIncreaseTenants: [WCRentIncreaseTenant] = []
         
         for tenant in allTenants {
@@ -193,8 +211,6 @@ class TenantDataManager: ObservableObject {
         }
         
         self.rentIncreaseTenants = rentIncreaseTenants
-//        print("Total Rent Increase Tenants: \(unitCount)")
-        return rentIncreaseTenants
     }
     
     func buildTranasactions(tenantID: Int) async {
@@ -232,111 +248,76 @@ class TenantDataManager: ObservableObject {
         print(paymentReversals.count)
         
     }
-}
-
-func makeLeaseTenants(tenant: RMTenant, lease: RMLease) -> WCLeaseTenant {
-    let leaseTenant = WCLeaseTenant(
-        accountGroupID: tenant.accountGroupID,
-        addresses: tenant.addresses,
-        allLeases: tenant.leases,
-        balance: tenant.balance,
-        charges: tenant.charges,
-        chargeTypes: tenant.chargeTypes,
-        checkPayeeName: tenant.checkPayeeName,
-        colorID: tenant.colorID,
-        comment: tenant.comment,
-        contacts: tenant.contacts,
-        createDate: tenant.createDate,
-        createUserID: tenant.createUserID,
-        defaultTaxTypeID: tenant.defaultTaxTypeID,
-        doNotAcceptChecks: tenant.doNotAcceptChecks,
-        doNotAcceptPayments: tenant.doNotAcceptPayments,
-        doNotAllowTWAPayments: tenant.doNotAllowTWAPayments,
-        doNotChargeLateFees: tenant.doNotChargeLateFees,
-        doNotPrintStatements: tenant.doNotPrintStatements,
-        doNotSendARAutomationNotifications: tenant.doNotSendARAutomationNotifications,
-        evictionID: tenant.evictionID,
-        failedCalls: tenant.failedCalls,
-        firstContact: tenant.firstContact,
-        firstName: tenant.firstName,
-        flexibleRentInternalStatus: tenant.flexibleRentInternalStatus,
-        flexibleRentStatus: tenant.flexibleRentStatus,
-        isAccountGroupMaster: tenant.isAccountGroupMaster,
-        isCompany: tenant.isCompany,
-        isProspect: tenant.isProspect,
-        isShowCommentBanner: tenant.isShowCommentBanner,
-        lastContact: tenant.lastContact,
-        lastName: tenant.lastName,
-        lastNameFirstName: tenant.lastNameFirstName,
-        lease: lease, // Set the single lease
-        loans: tenant.loans,
-        name: tenant.name,
-        openBalance: tenant.openBalance,
-        overrideCreateDate: tenant.overrideCreateDate,
-        overrideCreateUserID: tenant.overrideCreateUserID,
-        overrideReason: tenant.overrideReason,
-        overrideScreeningDecision: tenant.overrideScreeningDecision,
-        overrideUpdateDate: tenant.overrideUpdateDate,
-        overrideUpdateUserID: tenant.overrideUpdateUserID,
-        payments: tenant.payments,
-        paymentReversals: tenant.paymentReversals,
-        postingStartDate: tenant.postingStartDate,
-        propertyID: tenant.propertyID,
-        recurringChargeSummaries: tenant.recurringChargeSummaries,
-        rentDueDay: tenant.rentDueDay,
-        rentPeriod: tenant.rentPeriod,
-        screeningStatus: tenant.screeningStatus,
-        securityDepositHeld: tenant.securityDepositHeld,
-        securityDepositSummaries: tenant.securityDepositSummaries,
-        statementMethod: tenant.statementMethod,
-        status: tenant.status,
-        tenantDisplayID: tenant.tenantDisplayID,
-        tenantID: tenant.tenantID,
-        totalCalls: tenant.totalCalls,
-        totalEmails: tenant.totalEmails,
-        totalVisits: tenant.totalVisits,
-        udfs: tenant.udfs,
-        unit: lease.unit, // Use the unit from the lease
-        updateDate: tenant.updateDate,
-        updateUserID: tenant.updateUserID,
-        webMessage: tenant.webMessage,
-        primaryContact: tenant.primaryContact
-    )
     
-    return leaseTenant
+    func makeLeaseTenants(tenant: RMTenant, lease: RMLease) -> WCLeaseTenant {
+        let leaseTenant = WCLeaseTenant(
+            accountGroupID: tenant.accountGroupID,
+            addresses: tenant.addresses,
+            allLeases: tenant.leases,
+            balance: tenant.balance,
+            charges: tenant.charges,
+            chargeTypes: tenant.chargeTypes,
+            checkPayeeName: tenant.checkPayeeName,
+            colorID: tenant.colorID,
+            comment: tenant.comment,
+            contacts: tenant.contacts,
+            createDate: tenant.createDate,
+            createUserID: tenant.createUserID,
+            defaultTaxTypeID: tenant.defaultTaxTypeID,
+            doNotAcceptChecks: tenant.doNotAcceptChecks,
+            doNotAcceptPayments: tenant.doNotAcceptPayments,
+            doNotAllowTWAPayments: tenant.doNotAllowTWAPayments,
+            doNotChargeLateFees: tenant.doNotChargeLateFees,
+            doNotPrintStatements: tenant.doNotPrintStatements,
+            doNotSendARAutomationNotifications: tenant.doNotSendARAutomationNotifications,
+            evictionID: tenant.evictionID,
+            failedCalls: tenant.failedCalls,
+            firstContact: tenant.firstContact,
+            firstName: tenant.firstName,
+            flexibleRentInternalStatus: tenant.flexibleRentInternalStatus,
+            flexibleRentStatus: tenant.flexibleRentStatus,
+            isAccountGroupMaster: tenant.isAccountGroupMaster,
+            isCompany: tenant.isCompany,
+            isProspect: tenant.isProspect,
+            isShowCommentBanner: tenant.isShowCommentBanner,
+            lastContact: tenant.lastContact,
+            lastName: tenant.lastName,
+            lastNameFirstName: tenant.lastNameFirstName,
+            lease: lease, // Set the single lease
+            loans: tenant.loans,
+            name: tenant.name,
+            openBalance: tenant.openBalance,
+            overrideCreateDate: tenant.overrideCreateDate,
+            overrideCreateUserID: tenant.overrideCreateUserID,
+            overrideReason: tenant.overrideReason,
+            overrideScreeningDecision: tenant.overrideScreeningDecision,
+            overrideUpdateDate: tenant.overrideUpdateDate,
+            overrideUpdateUserID: tenant.overrideUpdateUserID,
+            payments: tenant.payments,
+            paymentReversals: tenant.paymentReversals,
+            postingStartDate: tenant.postingStartDate,
+            propertyID: tenant.propertyID,
+            recurringChargeSummaries: tenant.recurringChargeSummaries,
+            rentDueDay: tenant.rentDueDay,
+            rentPeriod: tenant.rentPeriod,
+            screeningStatus: tenant.screeningStatus,
+            securityDepositHeld: tenant.securityDepositHeld,
+            securityDepositSummaries: tenant.securityDepositSummaries,
+            statementMethod: tenant.statementMethod,
+            status: tenant.status,
+            tenantDisplayID: tenant.tenantDisplayID,
+            tenantID: tenant.tenantID,
+            totalCalls: tenant.totalCalls,
+            totalEmails: tenant.totalEmails,
+            totalVisits: tenant.totalVisits,
+            udfs: tenant.udfs,
+            unit: lease.unit, // Use the unit from the lease
+            updateDate: tenant.updateDate,
+            updateUserID: tenant.updateUserID,
+            webMessage: tenant.webMessage,
+            primaryContact: tenant.primaryContact
+        )
+        
+        return leaseTenant
+    }
 }
-
-
-/*
- // For Mail Labels
- var unitCount = 0
- for tenant in allTenants {
- if tenant.leases?.count ?? 0 > 1 {
- for lease in tenant.leases ?? [] {
- if lease.moveOutDate == nil && lease.unit?.unitType?.name != "Loan" {
- 
- print("\(tenant.name ?? "No Name") - \(tenant.tenantID ?? 0)")
- 
- if tenant.propertyID == 3 {
- print((lease.unit?.addresses?.first?.street ?? "").components(separatedBy: "\r\n").first ?? "")
- print((lease.unit?.addresses?.first?.street ?? "").components(separatedBy: "\r\n").last ?? "")
- } else {
- print(lease.unit?.addresses?.first?.street ?? "")
- }
- print(lease.unit?.addresses?.first?.city ?? "")
- print(lease.unit?.addresses?.first?.state ?? "")
- print(lease.unit?.addresses?.first?.postalCode ?? "")
- 
- print("\n")
- unitCount += 1
- print("Unit Count: \(unitCount)")
- 
- }
- }
- } else {
- unitCount += 1
- }
- }
- print(("Unit Count: \(unitCount)"))
- */
-
